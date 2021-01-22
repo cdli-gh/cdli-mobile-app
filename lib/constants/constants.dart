1dd4dbeacb0caf491e38a75d347c10978d23e6ea
pranjalgoyal <pranjalgoyal13@gmail.com>
2021-01-22 16:19:53 +0530

    Updated the constants folder, Added constant for AppBartitle

---
+++ lib/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const Widget kTextStyletitle = Text(
+  'cdli tablet',
+  style: TextStyle(
+    color: Colors.white,
+    fontFamily: 'NotoSansJP',
+    fontWeight: FontWeight.w400,
+  ),
+);
